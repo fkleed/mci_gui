08a7bdb5dc8a9b2f7def3b1e26ae7d8e6da4a2ea
Ferenc Kleedörfer <ferenc.kleedoerfer@outlook.de>
2022-06-20 17:21:54 +0200

    navigation styling update

--- components/navigation/Navigation.py
+++ components/navigation/Navigation.py
@@ -8,8 +8,8 @@ class Navigation:
 
     def __init__(self, master):
         frm_navigation = tk.Frame(master)
-        frm_navigation.columnconfigure(0, weight=1)
-        frm_navigation.columnconfigure(1, weight=3)
+        frm_navigation.columnconfigure(0, weight=0)
+        frm_navigation.columnconfigure(1, weight=1)
         frm_navigation.rowconfigure(0, weight=1)
         frm_navigation.rowconfigure (1, weight=0)
 
