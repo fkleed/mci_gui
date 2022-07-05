d3a5ec3846ed0d350fa9b6aa23b246de5f5f0729
Ferenc Kleedörfer <ferenc.kleedoerfer@outlook.de>
2022-07-05 09:17:30 +0200

    updated all and important messages

--- components/navigation/AllMessages.py
+++ components/navigation/AllMessages.py
@@ -1,6 +1,10 @@
 import tkinter as tk
+import sys
+
+sys.path.append('../shared')
 
 from components.navigation.MessagePreview import MessagePreview
+from shared.Message import Message
 
 class AllMessags(tk.Frame):
 
@@ -11,17 +15,19 @@ class AllMessags(tk.Frame):
         self.rowconfigure(0, weight=0)
         self.rowconfigure(1, weight=1)
 
-        self.all_messages_label = AllMessagesLabel(self, 5)
+        self.all_messages_label = AllMessagesLabel(self)
         self.all_messages_to_display = AllMessagesToDisplay(self)
+
+        self.all_messages_to_display.add_new_message(Message("Tony M", "Test"))
 
         self.grid(column=0, row=0, sticky="nsew")
 
 class AllMessagesLabel(tk.Frame):
 
-    def __init__(self, master, count):
+    def __init__(self, master):
         super().__init__(master)
 
-        self.all_messages_label_text = tk.Label(self, text="All messages (" + str(count) + ")")
+        self.all_messages_label_text = tk.Label(self, text="All messages")
         self.all_messages_label_text.configure(font=("Open Sans", 12, "bold"))
         self.all_messages_label_text.pack(side=tk.LEFT, padx=(10,10), pady=10)
 
@@ -31,11 +37,8 @@ class AllMessagesToDisplay(tk.Frame):
 
     def __init__(self, master):
         super().__init__(master)
-
-        for i in range(5):
-            if i == 0:
-                i = MessagePreview(self, "User", "Lorem Ipsum...", True)
-            else:
-                i = MessagePreview(self, "User", "Lorem Ipsum...", False)
-
         self.grid(column=0, row=1, sticky="nsew")
+
+    def add_new_message(self, message):
+        MessagePreview(self, message.sender, message.text, False)
+
--- components/navigation/ImportantMessages.py
+++ components/navigation/ImportantMessages.py
@@ -1,6 +1,10 @@
 import tkinter as tk
+import sys
+
+sys.path.append('../shared')
 
 from components.navigation.MessagePreview import MessagePreview
+from shared.Message import Message
 
 class ImportantMessages(tk.Frame):
 
@@ -11,17 +15,19 @@ class ImportantMessages(tk.Frame):
         self.rowconfigure(0, weight=0)
         self.rowconfigure(1, weight=1)
 
-        self.important_messages_label = ImportantMessagesLabel(self, 3)
+        self.important_messages_label = ImportantMessagesLabel(self)
         self.important_messages_to_display = ImportantMessagesToDisplay(self)
+
+        self.important_messages_to_display.add_important_message(Message("Guido W", "-Test"))
 
         self.grid(column=0, row=0, sticky="nsew")
 
 class ImportantMessagesLabel(tk.Frame):
 
-    def __init__(self, master, count):
+    def __init__(self, master):
         super().__init__(master)
 
-        self.important_messages_label_text = tk.Label(self, text="Important messages (" + str(count) + ")")
+        self.important_messages_label_text = tk.Label(self, text="Important messages")
         self.important_messages_label_text.configure(font=("Open Sans", 12, "bold"))
         self.important_messages_label_text.pack(side=tk.LEFT, padx=(10,10), pady=10)
 
@@ -31,11 +37,7 @@ class ImportantMessagesToDisplay(tk.Frame):
 
     def __init__(self, master):
         super().__init__(master)
-
-        for i in range(3):
-            if i == 0:
-                i = MessagePreview(self, "User", "Lorem Ipsum...", True)
-            else:
-                i = MessagePreview(self, "User", "Lorem Ipsum...", False)
-
         self.grid(column=0, row=1, sticky="nsew")
+
+    def add_important_message(self, message):
+        MessagePreview(self, message.sender, message.text, False)
